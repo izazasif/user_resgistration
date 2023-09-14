11ed68d1736541b7e2eb7798aad40eab4df41ceb
izazasif <asifizaz5@gmail.com>
2023-09-14 12:52:54 +0600

    last12314

--- resources/views/profile/pdf_template.blade.php
+++ resources/views/profile/pdf_template.blade.php
@@ -115,98 +115,56 @@
                         </table>
                 </th>
             </tr>
+        </table>   
+        <table >
+            <tr >
+                <th>
+                    <table>
+                        
+                            <tr>
+                                <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">HIGH SCHOOL:</th>
+                                <th style="padding-left: 20px;font-size: 16px;width:160px;text-align: justify;">{{ $profile->h_school }}</th>
+                            </tr>
+                            <tr>
+                                <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">GPA: </th>
+                                <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->gpa }} </th>
+                            </tr>
+                            <tr>
+                                <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">ACT:</th>
+                                <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->act }}</th>
+                            </tr>
+                            <tr>
+                                <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">MAJOR:</th>
+                                <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->major }}</th>
+                            </tr>
+                            <tr>
+                                <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">ACHIEVEMENTS:</th>
+                                <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->achievements }}</th>
+                            </tr>
+                    </table>
+                </th>
+                <th>   
+                    <table>
+                        <tr>
+                            <th style="padding-left: 20px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;vertical-align: top;">CLUB:</th>
+                            <th style="font-size: 16px;width:130px;text-align: justify;vertical-align: top;">{{ $profile->club }}</th>
+                        </tr>
+                        <tr>
+                            <th style="padding-left: 20px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;vertical-align: top;">POSITION: </th>
+                            <th style="font-size: 16px;width:80px;text-align: justify;">{{ $profile->position }} </th>
+                        </tr>
+                        <tr>
+                            <th style="padding-left: 20px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;vertical-align: top;">ACHIEVEMENTS:</th>
+                            <th style="font-size: 16px;width:130px;text-align: justify;vertical-align: top;">{{ $profile->c_achievements  }}</th>
+                        </tr>
+                            <p style="padding-left: 20px;font-size: 12px;font-weight: bold;color:#990000;margin: 2;">FOOTAGE :  <strong style="color:black;text-align: justify;"> {{ $profile->footage }} </strong></p>
+                           <p style="padding-left: 20px;font-size: 12px;font-weight: bold;color:#990000;width:190px;margin: 2;">HIGHR SCHOOL CONTACTS: <strong style="color:black;text-align: justify;"> {{ $profile->h_contact }} </strong> </p>
+                            <p style="padding-left: 20px;font-size: 12px;font-weight: bold;color:#990000;margin: 2;">ADITIONAL INFORMATION:<strong style="color:black;text-align: justify;"> {{ $profile->additional_information }} </strong></p> 
+                    </table>
+                </th>
+            </tr>
         </table>
-        
-    <table >
-        <tr>
-            <th>
-            <!-- <h2 style="color: red;padding-left: 100px;">ACADEMICS</h2>
-            <h2 style="border-bottom: 2px solid black;margin-left:130px;width:150px;margin-top:-20px;"></h2> -->
-               <table>
-                 
-                    <tr>
-                        <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">HIGH SCHOOL:</th>
-                        <th style="padding-left: 20px;font-size: 16px;width:160px;text-align: justify;">{{ $profile->h_school }}</th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;">GPA: </th>
-                        <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->gpa }} </th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;">ACT:</th>
-                        <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->act }}</th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;">MAJOR:</th>
-                        <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->major }}</th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 20px;font-size: 12px;width:80px;font-weight: bold;margin: 2;color:#990000;vertical-align: top;">ACHIEVEMENTS:</th>
-                        <th style="padding-left: 20px;font-size: 16px;width:80px;text-align: justify;">{{ $profile->achievements }}</th>
-                    </tr>
-              </table>
-            </th>
-            <th>   
-            <!-- <h2 style="color: #990000;padding-left: 60px;">ATHLETICS</h2>
-            <h2 style="border-bottom: 2px solid black;margin-left:140px;width:150px;margin-top:-20px;"></h2> -->
-                <table>
-                    <tr>
-                        <th style="padding-left: 40px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;vertical-align: top;">CLUB:</th>
-                        <th style="font-size: 16px;width:130px;text-align: left;vertical-align: top;">{{ $profile->club }}</th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 40px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;">POSITION: </th>
-                        <th style="font-size: 16px;width:80px;text-align: justify;">{{ $profile->position }} </th>
-                    </tr>
-                    <tr>
-                        <th style="padding-left: 40px;font-size: 12px;width:20px;font-weight: bold;margin: 2;color:#990000;text-align: left;vertical-align: top;">ACHIEVEMENTS:</th>
-                        <th style="font-size: 16px;width:130px;text-align: justify;vertical-align: top;">{{ $profile->c_achievements  }}</th>
-                    </tr>
-                     <p style="padding-left: 40px;font-size: 12px;font-weight: bold;color:#990000;">FOOTAGE :  <strong style="color:black;text-align: justify;"> {{ $profile->footage }} </strong></p>
-                        <p style="padding-left: 40px;font-size: 12px;font-weight: bold;color:#990000;width:190px;">HIGHR SCHOOL CONTACTS: <strong style="color:black;text-align: justify;"> {{ $profile->h_contact }} </strong> </p>
-                        <p style="padding-left: 40px;font-size: 12px;font-weight: bold;color:#990000;">ADITIONAL INFORMATION:<strong style="color:black;text-align: justify;"> {{ $profile->additional_information }} </strong></p>
-                </table>
-            </th>
-        </tr>
-    </table>
     </div>
-    <!-- <div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>Academics</h2>
-            <p><strong>High School: </strong>&nbsp; &nbsp;{{ $profile->h_school }}</p>
-            <p><strong>GPA: </strong>&nbsp; &nbsp;{{ $profile->gpa }}</p>
-            <p><strong>Act:</strong>&nbsp; &nbsp;{{ $profile->act }}</p>
-            <p><strong>Major:</strong>&nbsp; &nbsp;{{ $profile->major }}</p>
-            <p><strong>Achievements: </strong>&nbsp; &nbsp;{{ $profile->achievements }}</p>
-        </div>
-        <div class="col-md-6">
-            <h2>Athletics</h2>
-            <p><strong>Club: </strong>&nbsp; &nbsp;{{ $profile->club }}</p>
-            <p><strong>Position: </strong>&nbsp; &nbsp;{{ $profile->position }}</p>
-            <p><strong>club achievements:</strong>&nbsp; &nbsp;{{ $profile->c_achievements }}</p>
-            <p><strong>Footage :</strong>&nbsp; &nbsp;{{ $profile->footage }}</p>
-            <p><strong>High School Contact:</strong>&nbsp; &nbsp;{{ $profile->h_contact }}</p>
-            <p><strong>Aditional Information:</strong>&nbsp; &nbsp;{{ $profile->additional_information }}</p>
-        </div>
-    </div>
-  </div>
-
-   <td style="padding-left: 20px;">
-                
-            <h2 style="margin-left:90px;">ATHLETICS</h2>
-           
-            <p><strong>CLUB: </strong>&nbsp; &nbsp;{{ $profile->club }}</p>
-            <p><strong>POSITION: </strong>&nbsp; &nbsp;{{ $profile->position }}</p>
-            <p><strong>CLUB ACHIEVEMENTS:</strong>&nbsp; &nbsp;{{ $profile->c_achievements }}</p>
-            <p><strong>FOOTAGE :</strong>&nbsp; &nbsp;{{ $profile->footage }}</p>
-            <p><strong>HIGHR SCHOOL CONTACTS:</strong>&nbsp; &nbsp;{{ $profile->h_contact }}</p>
-            <p><strong>ADITIONAL INFORMATION:</strong>&nbsp; &nbsp;{{ $profile->additional_information }}</p>
-            </td>
--->
-       
-   
-
 </body>
 </html>
 
